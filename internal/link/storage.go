c198cf2fbc7102a29480f4b5e05f36acafe1bf4f
PabstBlue1386 <longer1386@gmail.com>
2025-11-13 22:08:56 +0500

    added struct storage

--- internal/link/storage.go
+++ internal/link/storage.go
@@ -1,1 +1,34 @@
 package link
+
+import (
+	"errors"
+	"time"
+)
+
+type Storage struct {
+	Links  []Link `json:"links"`
+	NextID int    `json:"nextID"`
+}
+
+func (storage *Storage) Add(link Link) (Link, error) {
+
+	if link.Title == "" {
+		return Link{}, errors.New("Empty title")
+	}
+	if link.URL == "" {
+		return Link{}, errors.New("Empty URL")
+	}
+	if link.Category == "" {
+		return Link{}, errors.New("Empty category")
+	}
+	if len(link.Tags) < 1 {
+		return Link{}, errors.New("Minimum one tag")
+	}
+
+	link.ID = storage.NextID
+	link.AddedAt = time.Now()
+
+	storage.Links = append(storage.Links, link)
+	storage.NextID++
+	return link, nil
+}
