3e253ae2de41c37c611f9ba5cebf38f6d1f03899
PabstBlue1386 <longer1386@gmail.com>
2025-11-21 08:17:24 +0500

    improved the adding function

--- internal/link/storage.go
+++ internal/link/storage.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"errors"
+	"strings"
 	"time"
 )
 
@@ -10,19 +11,55 @@ type Storage struct {
 	NextID int    `json:"nextID"`
 }
 
+func NewStorage() *Storage {
+	return &Storage{
+		Links:  make([]Link, 0),
+		NextID: 1,
+	}
+}
+
 func (storage *Storage) Add(link Link) (Link, error) {
 
+	link.Title = strings.Trim(link.Title, " ")
 	if link.Title == "" {
-		return Link{}, errors.New("Empty title")
+		return Link{}, errors.New("empty title")
+	} else {
+
 	}
+
+	link.URL = strings.TrimSpace(link.URL)
 	if link.URL == "" {
-		return Link{}, errors.New("Empty URL")
+		return Link{}, errors.New("empty URL")
+	} else {
+		for _, l := range storage.Links {
+			if strings.EqualFold(l.URL, link.URL) {
+				return Link{}, errors.New("existing URL")
+			}
+		}
 	}
+
+	link.Category = strings.Trim(link.Category, " ")
 	if link.Category == "" {
-		return Link{}, errors.New("Empty category")
+		return Link{}, errors.New("empty category")
+	} else {
+
 	}
+
 	if len(link.Tags) < 1 {
-		return Link{}, errors.New("Minimum one tag")
+		return Link{}, errors.New("minimum one tag")
+	} else {
+		var result []string
+		for _, tag := range link.Tags {
+			t := strings.TrimSpace(tag)
+			t = strings.ToUpper(t)
+			if t != "" {
+				result = append(result, t)
+			}
+		}
+		if len(result) < 1 {
+			return Link{}, errors.New("minimum one tag")
+		}
+		link.Tags = result
 	}
 
 	link.ID = storage.NextID
